093eb74cbda8edc90e623f740367d638be726eaf
mustafijar <mustafijarrahanammoon@gmail.com>
2026-09-26 01:32:38 +0600

    Update portfolio

--- src/Components/AboutMe/About.jsx
+++ src/Components/AboutMe/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
         {/* About Content */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
 
-          {/* Left Side */}
+          {/* Left Sde */}
           <div>
 
             <h3 className="text-3xl font-bold text-gray-900 mb-5">
